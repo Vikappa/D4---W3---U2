8bbe8c16d4035d61fc1460d81b9b17179c16c676
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-18 16:35:03 +0100

    parte 1

--- js/script.js
+++ js/script.js
@@ -2,18 +2,29 @@ const firstButton = document.getElementById('firstButton')
 const secondButton = document.getElementById('secondButton')
 
 const myURL = `https://api.pexels.com/v1/search?query=`
+const screenCards = []
+
+const refreshScreen = function () {
+    console.log(screenCards)
+    document.getElementById('mainRow').innerHTML = ``
+    let innerHtmlCards = ``
+    for (let index = 0; index < screenCards.length; index++) {
+        innerHtmlCards = innerHtmlCards + screenCards[index]
+    }
+
+    document.getElementById('mainRow').innerHTML = innerHtmlCards
+}
+
 
 
 const getPicWithQuery = function (query) {
-    console.log(query)
 
-    fetch(myURL, {
+    fetch(myURL + query, {
         headers: {
             Authorization: "KRnL0eTtRPS9t81c0qjpP4DTgkXjne0537FX088gqlinFRDNqMKitPwa"
         }
     })
         .then((response) => {
-            console.log('response', response)
             if (response.ok) {
                 return response.json()
             } else {
@@ -21,8 +32,17 @@ const getPicWithQuery = function (query) {
             }
         })
         .then((data) => {
-            console.log('DATA', data)
-            console.log(data)
+            console.log(data.photos[Math.floor(Math.random() * 16)])
+            console.log(data.photos[Math.floor(Math.random() * 16)].src)
+
+
+            screenCards.push(
+                buildCard(data.photos[Math.floor(Math.random() * 16)].alt,
+                    data.photos[Math.floor(Math.random() * 16)].src.medium)
+
+            )
+            refreshScreen()
+            return data
         })
         .catch((err) => {
             console.log(err)
@@ -30,21 +50,55 @@ const getPicWithQuery = function (query) {
 
 }
 
+const buildCard = function (title = "Lorem Ipsum", src = "https://picsum.photos/id/237/300/200") {
+    let mockup =
+        `<div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+        <img src="${src}" class="bd-placeholder-img card-img-top" />
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                        View
+                    </button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                        Edit
+                    </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+            </div>
+        </div>
+    </div>
+</div>`
 
+    return mockup
+}
 
-getConcerts()
+const pageFirstLoad = function () {
+    let innerHtmlCards = ``
+    for (let index = 0; index < 15; index++) {
+        screenCards.push(buildCard())
+        innerHtmlCards = innerHtmlCards + screenCards[index]
+    }
 
-
+    document.getElementById('mainRow').innerHTML = innerHtmlCards
+}
 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 firstButton.addEventListener('click', function () {
-    let primaquery = "Ufo"
-    getPicWithQuery(primaquery)
+    getPicWithQuery("Ufo")
 })
 
 secondButton.addEventListener('click', function () {
-    let secondaQuery = "Aliens"
-
+    getPicWithQuery("Aliens")
 })
+
+pageFirstLoad()
